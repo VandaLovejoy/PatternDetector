36bd9154606a5e57f666297beda85de81109d40b
VandaLovejoy <vanda.gaonach-lovejoy@umontreal.ca>
2021-02-18 16:06:06 -0500

    multiplethreads

--- src/MafScanCcr.java
+++ src/MafScanCcr.java
@@ -148,10 +148,10 @@ public class MafScanCcr {
                  ****   RNALalifold       ****
                  ************************************************************************/
 
-                String cmd = ALIFOLDBINARY + " --id-prefix=alifold" + " --noLP" + " --maxBPspan=200"+ " --ribosum_scoring"
+                String cmd = ALIFOLDBINARY + " --id-prefix=alifold" + " --noLP" + " --maxBPspan=200" + " --ribosum_scoring"
                         + " --aln-stk " + Args[Args.length - 1];
 
-                executeCommand(cmd);
+                 //executeCommand(cmd);
 
 
                 ReadFile = new BufferedReader(new FileReader(Args[i]));
@@ -162,6 +162,9 @@ public class MafScanCcr {
                 /************************************************************************
                  ****   This stuff is messy, but avoids problems at last block       ****
                  ************************************************************************/
+
+                ExecutorService MultiThreads = Executors.newFixedThreadPool(NTHREDS);
+                List<Future<Runnable>> futures = new ArrayList<Future<Runnable>>();
                 readBlocks:
                 while ((Line = ReadFile.readLine()) != null || newLineCount == lineCount) {
                     if (Line == null || Line.length() > 1 && Line.charAt(0) != '#')
@@ -177,30 +180,156 @@ public class MafScanCcr {
 
                             if (Temp.split("@").length >= 3) { // at least 3 sequences
                                 TempTab = new String[Temp.split("@").length];
-                                TempTab = Temp.split("@");Temp = "";
-                                
-                                AssociativeList species = new AssociativeList();
+                                TempTab = Temp.split("@");
+                                Temp = "";
+
+                                ArrayList<String[]> associativeList = new ArrayList<>();
                                 mafTabTemp = TempTab[0].split("\\s+");
+                                futures = new ArrayList<Future<Runnable>>();
+                                // add Path Flag ? < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+                                String Path = OUT_PATH + "/aln/" + mafTabTemp[1].substring(mafTabTemp[1].lastIndexOf(".") + 1);
+                                if (!(new File(Path)).isDirectory())
+                                    (new File(Path)).mkdirs();
+                                int numbZeros = 0;
+                                String gcReference = "";
+                                String gcSScons = "";
+
+                                HashMap<String[], ArrayList> motifs = new HashMap<>();
+                                String lastDigit = "";
+                                String finalName = "";
+                                lastDigit += String.valueOf(blockAln);
+                                numbZeros = 4 - lastDigit.length();
+                                int x = 0;
+                                while (x < numbZeros) {
+                                    finalName += String.valueOf(0);
+                                    x++;
+                                }
+                                finalName += lastDigit;
+
+                                File file = new File(dirProgram + "/" + OUT_PATH + "/stockholm" + "/alifold_"
+                                        + finalName + ".stk");
 
 
+                                try {
 
-                                HashMap<String[], ArrayList> motifs = species.addSpecies(dirProgram + "/" + OUT_PATH,
-                                        blockAln, mafTabTemp);
-                                /*for (HashMap.Entry<String[], ArrayList> entry : motifs.entrySet()) {
-                                    Iterator iter = entry.getValue().iterator();
-                                    ArrayList<char[]> alnTab = new ArrayList<>();
-                                    while (iter.hasNext()) {
-                                        String[] line = (String[]) iter.next();
-                                        char[] aln = species.getAlnTab(line);
 
-                                        alnTab.add(aln);
+                                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                                    String currentLine = reader.readLine();
 
+                                    String[] arrayName = new String[5];
+                                    int lengthAln = 0;
+
+                                    readAlns:
+
+                                    while (currentLine != null) {
+
+
+                                        if (currentLine.startsWith("#=GF ID ")) {
+                                            arrayName = currentLine.split("[_.]");
+                                            lengthAln = Integer.parseInt(arrayName[4]) -
+                                                    Integer.parseInt(arrayName[3]);
+                                            associativeList = new ArrayList<>();
+                                        } else if (currentLine.startsWith("#=GC RF")) {
+                                            String[] lineReference = currentLine.split(" ");
+                                            gcReference = lineReference[lineReference.length - 1];
+                                        } else if (currentLine.startsWith("#=GC SS_cons")) {
+                                            String[] lineReference = currentLine.split(" ");
+                                            gcSScons = lineReference[lineReference.length - 1];
+                                        } else if (currentLine.startsWith("#") || currentLine.equals("")) {
+                                            currentLine = reader.readLine();
+                                            continue readAlns;
+
+                                        } else if (lengthAln > 50 && !(currentLine.startsWith("//"))) {
+
+                                            String[] species = currentLine.split(" ", 2);
+                                            species[1] = species[1].trim();
+
+                                            associativeList.add(species);
+                                        }
+                                        if ((!associativeList.isEmpty()) && currentLine.startsWith("//")) {
+                                            int[] cordMotif = getRealCoordinates(Integer.parseInt(arrayName[3])
+                                                    , mafTabTemp, associativeList.get(0)[1]);
+                                            String loci = Arrays.toString(cordMotif);
+                                            String chrom = mafTabTemp[1].substring((mafTabTemp[1].lastIndexOf(".")) + 1);
+                                            String lociChrm = chrom + ", " + loci.substring(1, loci.length() - 1) + ", " +
+                                                    mafTabTemp[4] + ", " + arrayName[3] + ", " + arrayName[4] + ", " + gcReference + ", "
+                                                    + gcSScons;
+                                            String[] arrayLociChrm = lociChrm.split(", ");
+
+
+                                            Iterator iter = associativeList.iterator();
+                                            ArrayList<char[]> alnTab = new ArrayList<>();
+                                            while (iter.hasNext()) {
+                                                String[] line = (String[]) iter.next();
+                                                char[] aln = line[1].toCharArray();
+                                                alnTab.add(aln);
+                                            }
+
+                                            /*ScanItFast Block = new ScanItFast( associativeList, alnTab,
+                                                    arrayLociChrm,Path,dirProgram + "/" + OUT_PATH, GAPS, SSZBINARY, RSCAPEBINARY, VERBOSE, PRINTALL);*/
+                                            /*Block.setSsz(SSZ);
+                                            Block.setSszR(SSZR);*/
+                                            Future f = MultiThreads.submit(new ScanItFast( associativeList, alnTab,
+                                                    arrayLociChrm,Path,dirProgram + "/" + OUT_PATH, GAPS, SSZBINARY, RSCAPEBINARY, VERBOSE, PRINTALL));
+                                            futures.add(f);
+                                        }
+                                        currentLine = reader.readLine();
+                                        continue readAlns;
                                     }
 
-                                    SplitNfold(mafTabTemp, entry.getValue(), alnTab, entry.getKey());
+
+                                    reader.close();
 
 
-                                }*/
+                                    //  file.delete();
+
+
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                 /*File index = new File(dirProgram + "/"+ OUT_PATH +"/R-Scape");
                                 String[] entriesRscape = index.list();
                                 for (String s: entriesRscape){
@@ -208,34 +337,90 @@ public class MafScanCcr {
                                     currentFile.delete();
                                 }
 */
-                                blockAln ++;
-                            } else if (Temp.split("@").length == 2) {
-                                blockAln ++;
+                                for (Future<Runnable> g : futures) {
+                                    try{
+                                    g.get();
+
+
+                            } catch (Exception Fuck) {
+                                System.err.println("MultiThreads took too long!  OOps!");
+                                Fuck.printStackTrace();
                             }
+                                }
 
 
-
-
-
-
-
-
-
-                            Temp = "";
-                    }
+                            blockAln++;
+                        } else if (Temp.split("@").length == 2) {
+                            blockAln++;
                         }
-                    // Wait until all threads are finished
+
+
+                        Temp = "";
+                    }
                 }
+                // Wait until all threads are finished
+            }
+
 
                 ReadFile.close();
+                MultiThreads.shutdown();
+                MultiThreads.awaitTermination(60 * 10L, TimeUnit.SECONDS);
 
             }
         }
 
 
-        long endTime = System.nanoTime();
-        System.out.println("That took"+ (endTime-startTime) +" for progrom to finish in nanoseconds");
+        /*long endTime = System.nanoTime();
+        System.out.println("That took"+ (endTime-startTime) +" for progrom to finish in nanoseconds");*/
     }
+
+    //*********************************************************************
+    //					Alignment generator				*
+    //*********************************************************************
+
+
+
+    private static int[] getRealCoordinates (int start, String[] mafCord, String motifHuman){
+        int nuc = 0;
+        int [] cordFinal;
+        for (int i = 0; i < start; i++ ){
+            char gap = mafCord[6].charAt(i);
+            if (!(gap == '-')){
+                nuc+=1;
+            }
+        }
+
+        if (mafCord[4].equals("-")){
+            int lociEnd = (Integer.parseInt(mafCord[5] ) + 1  - (Integer.parseInt(mafCord[2]) + nuc)) + 1 ;
+
+            int lociStart = lociEnd - motifHuman.replaceAll("[^ATCGUatcgu]", "").length();
+            cordFinal = new int[]{lociStart, lociEnd};
+        } else {
+            int lociStart = Integer.parseInt(mafCord[2]) + nuc;
+            int lociEnd = lociStart + (motifHuman.length());
+            cordFinal = new int[]{lociStart, lociEnd};
+        }
+
+        return cordFinal;
+    }
+    protected static char[] getAlnTab(String[] seq){
+        char[] AlnTab = new char[seq[0].length()];
+        if (seq.length>1 && seq[1].length()> 10) {
+            AlnTab = seq[1].toCharArray();
+        }
+        return AlnTab;
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
